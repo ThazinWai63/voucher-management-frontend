188f330bdbd24e912a631ef4043a25d3e8339f8a
KhinYadanarNg <e0015793@u.nus.edu>
2024-02-04 15:03:16 +0800

    format for login page

--- app/components/authentication/Login.tsx
+++ app/components/authentication/Login.tsx
@@ -2,42 +2,42 @@
 import React, { useState } from 'react'
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isValidEmail, setValidEmail] = useState(false);
-  
-    const logIn = () => {
-      validateEmail()
-      if (password.length > 0 && isValidEmail) {
-           
-      } else{
-       
-      }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isValidEmail, setValidEmail] = useState(false);
+
+  const logIn = () => {
+    validateEmail()
+    if (password.length > 0 && isValidEmail) {
+
+    } else {
+
     }
-  
-    const validateEmail = () => {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      const isValid = emailRegex.test(email);
-      setValidEmail(isValid);
-    }
-  
-    return (
-      <main>
-        <div className="mt-20">
-          <h1 className="text-4x1 font-extrabold text-center">Welcome to IV Voucher
-          </h1>
-        </div>
-        <div className="mt-10">
-          <form className='wrapper' action="/" method="post">
-            <input type="text" placeholder="Please enter email" className="logintext__input" 
-            onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" placeholder="Please enter password" className="logintext__input mt-10"
+  }
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(email);
+    setValidEmail(isValid);
+  }
+
+  return (
+    <main>
+      <div className="mt-20">
+        <h1 className="text-4x1 font-extrabold text-center">Welcome to IV Voucher
+        </h1>
+      </div>
+      <div className="mt-10">
+        <form className='wrapper' action="/" method="post">
+          <input type="text" placeholder="Please enter email" className="logintext__input"
+            onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="Please enter password" className="logintext__input mt-10"
             onChange={(e) => setPassword(e.target.value)} />
-            <button type="button" className="authentication-btn mt-10" onClick={logIn}>Login</button>
-      </form>
-        </div >
-      </main >
-    );
+          <button type="button" className="authentication-btn mt-10" onClick={logIn}>Login</button>
+        </form>
+      </div >
+    </main >
+  );
 }
 
 export default Login
