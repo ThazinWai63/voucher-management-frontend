6c9b30dde9f8e7978ca67a815539ff2d1f078d11
KhinYadanarNg <e0015793@u.nus.edu>
2024-02-07 00:35:06 +0800

    check proper validation for registration page

--- app/components/authentication/Registration.tsx
+++ app/components/authentication/Registration.tsx
@@ -2,6 +2,7 @@
 import { Listbox, Transition } from '@headlessui/react'
 import React, { Fragment, useState } from 'react'
 import CustomListBox from '../CustomListBox';
+import { isValidateEmail } from '@/utils';
 
 const Registration = () => {
 
@@ -11,7 +12,41 @@ const Registration = () => {
         { id: 2, type: 'Merchant' }
     ]
 
-    const [selectedUserType, setSelectedUserType] = useState(userTypes[0])
+    const [selectedUserType, setSelectedUserType] = useState(userTypes[0]);
+    const [password, setPassword] = useState('');
+    const [confirmedPassword, setConfirmedPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const signUp = () => {
+
+        isEmpty();
+
+        const isValidEmail = isValidateEmail(email);
+        if(!isValidEmail) {
+            alert('Please provide valid email');
+            return;
+        }
+
+        if(password !== confirmedPassword){
+            alert('Password and confirmed password do not match.');
+            return;
+        }
+       
+    }
+
+ 
+    const isEmpty = () => {
+        var elements = document.getElementsByTagName("input")
+        for (var i = 0; i < elements.length; i++) {
+            if (elements[i].value == "") {
+                alert('Please provide ' + elements[i].id);
+                return;
+            }
+        }
+    }
+
+
+
     return (
         <main>
             <div className="mt-20">
@@ -23,24 +58,24 @@ const Registration = () => {
                     <div className='registration__input'>
                         <text>Name</text>
                     </div>
-                    <input type="text" className="logintext__input" />
+                    <input type="text" id="name" className="logintext__input" />
                     <div className='registration__input'>
                         <text>Email</text>
                     </div>
-                    <input type="text" className="logintext__input" />
+                    <input type="text" id="email" className="logintext__input" onChange={(e) => setEmail(e.target.value)}/>
                     <div className='registration__input'>
                         <text>Password</text>
                     </div>
-                    <input type="password" className="logintext__input" />
+                    <input type="password" id="password" className="logintext__input" onChange={(e) => setPassword(e.target.value)}/>
                     <div className='registration__input'>
                         <text>Confirm Password</text>
                     </div>
-                    <input type="password" className="logintext__input" />
+                    <input type="password" id="confirmed password" className="logintext__input" onChange={(e) => setConfirmedPassword(e.target.value)}/>
                     <div className='registration__input pb-1'>
                         <text>Register as</text>
                     </div>
-                   <CustomListBox></CustomListBox>
-                    <button type="button" className="authentication-btn mt-10">Sign Up</button>
+                    <CustomListBox></CustomListBox>
+                    <button type="button" className="authentication-btn mt-10" onClick={signUp}>Sign Up</button>
                 </form>
             </div>
         </main>
---
+++ utils/index.ts
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const isValidateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(email);
+    return isValid;
+}
